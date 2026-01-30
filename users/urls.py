b0f0f412456a15e4e9431bc73969fbaf5037896f
Ayush Rusiya <ayushrusiya9@gmail.com>
2026-01-30 22:39:50 +0530

    implementing-jwt

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path,include
 from . import views
-
 from .views import UserViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # check token validity
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
